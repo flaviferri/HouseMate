d719b9d9f4ea3e8b17fb8bb483dd6dfa1379dfd7
“Flavia <flaviferri90@gmail.com>
2024-09-03 11:39:19 +0200

    CRUDE logic finished for task

--- src/main/java/com/houseMate/houseMate/models/User.java
+++ src/main/java/com/houseMate/houseMate/models/User.java
@@ -1,4 +1,39 @@
 package com.houseMate.houseMate.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Getter @Setter
+@Entity
+@Table(name="user")
 public class User {
+
+    public int id;
+    public String name;
+    public String lastName;
+    public String email;
+    public String password;
+    public LocalDate birth_day;
+    public LocalDate entry_date;
+    public LocalDate departure_date;
+
+    public User(){}
+    public User(int id , String name, String lastName, String email, String password, LocalDate birth_day, LocalDate entry_date, LocalDate departure_date){
+
+        this.id = id;
+        this.name= name;
+        this.lastName= lastName;
+        this.email=email;
+        this.password= password;
+        this.birth_day= birth_day;
+        this.entry_date= entry_date;
+        this.departure_date=departure_date;
+
+    }
+
 }
--- src/main/java/com/houseMate/houseMate/services/TaskService.java
+++ src/main/java/com/houseMate/houseMate/services/TaskService.java
@@ -1,4 +1,47 @@
 package com.houseMate.houseMate.services;
 
+import com.houseMate.houseMate.models.Task;
+import com.houseMate.houseMate.repositories.TaskRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootApplication
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getTasks(){
+        return taskRepository.findAllByOrderByIdAsc();
+    }
+
+    public Optional<Task> getTaskById(int id){
+        return taskRepository.findById(id);
+    }
+
+    public void saveTask (Task task){
+        taskRepository.save(task);
+    }
+
+    public boolean updateTask ( int id, Task task){
+        if (taskRepository.existsById(id)){
+            task.setId(id);
+            taskRepository.save(task);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteTask ( int id){
+        if ( taskRepository.existsById(id)){
+            taskRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
