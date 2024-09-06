a19861f30652703c01a3f4bd89f05dbc4a4a5f57
“Flavia <flaviferri90@gmail.com>
2024-09-06 19:33:48 +0200

    Fix:refactorTaskEnpoints

--- src/main/java/com/houseMate/houseMate/controllers/TaskController.java
+++ src/main/java/com/houseMate/houseMate/controllers/TaskController.java
@@ -2,14 +2,10 @@ package com.houseMate.houseMate.controllers;
 
 import com.houseMate.houseMate.models.Task;
 import com.houseMate.houseMate.services.ITaskService;
-import com.houseMate.houseMate.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,40 +26,28 @@ public class TaskController {
     }
 
 
-    @PostMapping("/create")
+    @PostMapping("/task/create")
     public ResponseEntity<Object> saveTask(@RequestBody Task task) {
         return taskServ.saveTask(task);
 
     }
 
-    @CrossOrigin(origins = "http://localhost:4001")
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteTask(@PathVariable("id") int id) {
-        if (taskServ.deleteTask(id)) {
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.notFound().build();
-    }
-
-    @CrossOrigin(origins = "http://localhost:4001")
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateTask(@PathVariable("id") int id, @RequestBody Task task) {
-        if (taskServ.updateTask(id, task)) {
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.notFound().build();
+    @DeleteMapping("/task/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable("id") int id) {
+        return taskServ.deleteTask(id);
 
     }
 
-
     @CrossOrigin(origins = "http://localhost:4001")
+    @PutMapping("/task/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable("id") int id, @RequestBody Task task) {
+        return taskServ.updateTask(id, task);
+    }
+
+
     @GetMapping("/task/{id}")
     public ResponseEntity<Object> getTaskById(@PathVariable("id") int id) {
-        Optional<Task> task = taskServ.getTaskById(id);
-        if (task.isPresent()) {
-            return ResponseEntity.ok(task.get());
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found");
+        return taskServ.getTaskById(id);
     }
 }
 
--- src/main/java/com/houseMate/houseMate/services/ITaskService.java
+++ src/main/java/com/houseMate/houseMate/services/ITaskService.java
@@ -3,16 +3,15 @@ package com.houseMate.houseMate.services;
 import com.houseMate.houseMate.models.Task;
 import org.springframework.http.ResponseEntity;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface ITaskService {
 
     List<Task> getTasks();
-    Optional<Task> getTaskById(int id);
+    ResponseEntity<Object> getTaskById(int id);
     ResponseEntity<Object> saveTask(Task task);
-    boolean updateTask(int id, Task task);
-    boolean deleteTask(int id);
+    ResponseEntity<Task> updateTask(int Task, Task task);
+    ResponseEntity<Void> deleteTask(int id);
 
 }
